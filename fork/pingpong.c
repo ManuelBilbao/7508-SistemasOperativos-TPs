e79e8969bb3955563a43a971f8493a35583fa026
Manu <bilbaomanuel98@gmail.com>
2021-09-22 15:34:35 -0300

    Ejercicios obligatorios

--- fork/pingpong.c
+++ fork/pingpong.c
@@ -1,7 +1,86 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+#define READ_INDEX 0
+#define WRITE_INDEX 1
+
+#define error(msg) \
+	do { \
+		perror(msg); \
+		exit(-1); \
+	} while (0);
+
 int
 main(void)
 {
-	// Your code here
+	srand(time(NULL));
+	int fdPadreHijo[2]; // Padre --> Hijo
+	int fdHijoPadre[2]; // Hijo --> Padre
+
+	printf("Hola, soy PID %d:\n", getpid());
+
+	if (pipe(fdPadreHijo) < 0)
+		error("Error al abrir un pipe");
+
+	if (pipe(fdHijoPadre) < 0)
+		error("Error al abrir un pipe");
+
+	printf("  - primer pipe me devuelve: [%i, %i]\n", fdPadreHijo[0], fdPadreHijo[1]);
+	printf("  - segundo pipe me devuelve: [%i, %i]\n\n", fdHijoPadre[0], fdHijoPadre[1]);
+
+	int i_fork = fork();
+
+	if (i_fork < 0)
+		error("Error al crear un nuevo proceso");
+
+	if (i_fork == 0) {  // Hijo
+		// Cierro la escritura del pipe Padre-->Hijo y la lecutra del pipe Hijo-->Padre
+		close(fdPadreHijo[WRITE_INDEX]);
+		close(fdHijoPadre[READ_INDEX]);
+
+		long int lectura;
+		if (read(fdPadreHijo[READ_INDEX], &lectura, sizeof(lectura)) < 0)
+			error("[Hijo] Error al leer del pipe");
+
+		printf("Donde fork me devuelve 0:\n");
+		printf("  - getpid me devuelve: %i\n", getpid());
+		printf("  - getppid me devuelve: %i\n", getppid());
+		printf("  - recibo valor %li vía fd=%i\n", lectura, fdPadreHijo[READ_INDEX]);
+		printf("  - reenvío valor en fd=%i y termino\n\n", fdHijoPadre[WRITE_INDEX]);
+
+		if (write(fdHijoPadre[WRITE_INDEX], &lectura, sizeof(lectura)) < 0)
+			error("[Hijo] Error al escribir en el pipe");
+
+		close(fdPadreHijo[READ_INDEX]);
+		close(fdHijoPadre[WRITE_INDEX]);
+	} else {  // Padre
+		// Cierro la lectura del pipe Padre-->Hijo y la escritura del pipe Hijo-->Padre
+		close(fdPadreHijo[READ_INDEX]);
+		close(fdHijoPadre[WRITE_INDEX]);
+
+		long int randNum = random();
+		long int lectura;
+
+		printf("Donde fork me devuelve %i:\n", i_fork);
+		printf("  - getpid me devuelve: %i\n", getpid());
+		printf("  - getppid me devuelve: %i\n", getppid());
+		printf("  - random me devuelve: %li\n", randNum);
+		printf("  - envío valor %li a través de fd=%i\n\n", randNum, fdPadreHijo[WRITE_INDEX]);
+
+		if (write(fdPadreHijo[WRITE_INDEX], &randNum, sizeof(randNum)) < 0)
+			error("[Padre] Error al escribir en el pipe");
+
+		if (read(fdHijoPadre[READ_INDEX], &lectura, sizeof(lectura)) < 0)
+			error("[Padre] Error al leer del pipe");
+
+		printf("Hola, de nuevo PID %i:\n", getpid());
+		printf("  - recibí valor %li vía fd=%i\n", lectura, fdHijoPadre[READ_INDEX]);
+
+		close(fdPadreHijo[WRITE_INDEX]);
+		close(fdHijoPadre[READ_INDEX]);
+	}
 
 	return 0;
 }
--- fork/primes.c
+++ fork/primes.c
@@ -1,7 +1,108 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define READ_INDEX 0
+#define WRITE_INDEX 1
+
+#define error(msg) \
+	do { \
+		perror(msg); \
+		exit(-1); \
+	} while (0);
+
 int
 main(int argc, char *argv[])
 {
-	// Your code here
+	int max = 10;
+	if (argc == 2) {
+		max = atoi(argv[1]);
+	}
+
+	int fd_izq[2], fd_der[2];
+	if (pipe(fd_izq) < 0)
+		error("Error al crear un pipe");
+
+	int i_fork = fork();
+	if (i_fork < 0)
+		error("Error al crear un nuevo proceso");
+
+	if (i_fork > 0) {  // Primero
+		// El primero se comunica a su "derecha"
+		fd_der[0] = fd_izq[0];
+		fd_der[1] = fd_izq[1];
+		close(fd_der[READ_INDEX]);
+
+		for (int i = 2; i <= max; i++) {
+			// Se podria optimizar y que el primero ya sea un filtro de 2
+			if (write(fd_der[WRITE_INDEX], &i, sizeof(i)) < 0)
+				error("Error al enviar un numero al segundo proceso");
+		}
+
+		close(fd_der[WRITE_INDEX]);
+
+		if (wait(NULL) < 0)
+			error("Error al hacer wait");
+	} else {  // Siguientes
+		close(fd_izq[WRITE_INDEX]);
+
+		ssize_t leidos;
+		int numero_leido;
+		int filtro;
+		bool primero = true;
+
+		while ((leidos = read(fd_izq[READ_INDEX], &numero_leido, sizeof(numero_leido))) > 0) {
+			if (!primero) {
+				// Mando los números al hijo
+				if (numero_leido % filtro != 0) {
+					if (write(fd_der[WRITE_INDEX], &numero_leido, sizeof(numero_leido)) < 0)
+						error("Error al enviar un número al siguiente proceso");
+				}
+				continue;
+			}
+
+			// Solo la primera vez del proceso:
+
+			filtro = numero_leido;
+			printf("primo %i\n", numero_leido);
+
+			// Creo un pipe y un proceso a la "derecha"
+			if (pipe(fd_der) < 0)
+				error("Error al crear un pipe");
+
+			int i = fork();
+			if (i < 0)
+				error("Error al crear un nuevo proceso");
+
+			if (i == 0) {  // Hijo
+				// Cierro la lectura del pipe que hereda con el abuelo
+				close(fd_izq[READ_INDEX]);
+
+				// El pipe "derecho" del padre es el "izquierdo" del hijo
+				fd_izq[0] = fd_der[0];
+				fd_izq[1] = fd_der[1];
+
+				close(fd_izq[WRITE_INDEX]);  // Cierro la escritura del pipe "izquierdo" en el hijo
+			} else {
+				primero = false;
+				close(fd_der[READ_INDEX]);  // Cierro la lectura del pipe "derecho" en el padre
+			}
+		}
+
+		if (leidos < 0)
+			error("Error al leer del pipe");
+
+		// Cierro la lectura del pipe "izquierdo" y la escritura del "derecho"
+		close(fd_izq[READ_INDEX]);
+		close(fd_der[WRITE_INDEX]);
+
+		// Si fd_izq != fd_der => se creó un proceso hijo al cual hay que esperar
+		if (fd_izq[READ_INDEX] != fd_der[READ_INDEX] && wait(NULL) < 0)
+			error("Error al hacer wait");
+	}
 
 	return 0;
 }
